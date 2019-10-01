2dd68f4cbe75a766cdd5d226ebab5eb50b14a327
Naved Khan <n@nvdk.co>
2019-10-01 15:27:18 +0530

    Rename project

--- csa/urls/api.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from csa import views
-
-urlpatterns = [
-
-
-]
--- csa/urls/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from csa import views
-
-urlpatterns = [
-    url(r'^dash/home/$', views.DashboardView.as_view(), name="csa-dashboard")
-]
