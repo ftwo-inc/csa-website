07100ad712696b1424e737a040ab1f7659c1cd82
khushboo2002 <khushboo@sqre1.com>
2019-10-01 17:33:35 +0530

    resolve conflict

--- Dockerfile
+++ Dockerfile
@@ -15,11 +15,7 @@ COPY . /code/
 # Expose port
 EXPOSE 8080
 
-<<<<<<< HEAD
-RUN ["python", "manage.py", "collectstatic"]
-=======
 # RUN ["python", "manage.py", "collectstatic", "--no-input"]
 # This is too slow to be done on GCB. Manual for now.
->>>>>>> 4cf109ac867bfbd42d10c5920a81da7dec556e6d
 
 ENTRYPOINT ["bash", "./entrypoint.sh"]
--- website/views.py
+++ website/views.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from rest_framework import status
 from django.views.generic import TemplateView
-from django.views import generic
-from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
-from rest_framework.views import APIView
 
 
 class HomePageView(TemplateView):
