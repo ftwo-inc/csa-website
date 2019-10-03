d1286d59651856de15fa06eb69b4f3c97b53a7ac
khushboo2002 <khushboo@sqre1.com>
2019-10-03 12:06:17 +0530

    url's fixed

--- csa/urls/urls.py
+++ csa/urls/urls.py
@@ -1,6 +1,17 @@
 from django.conf.urls import url
 from csa import views
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^dash/home/$', views.DashboardView.as_view(), name="csa-dashboard")
+    url(r'^dash/home/$', views.DashboardView.as_view(), name="csa-dashboard"),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('csa.urls.api'), name="api"),
+    url(r'^', include('website.urls.urls'), name="website-page"),
 ]
+
+if settings.DEBUG:
+
+    urlpatterns += static('/static', document_root=settings.STATIC_ROOT)
