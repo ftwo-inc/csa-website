3ef45e5665bbb1a40f6500e0d40bdf2e5ffdf4cf
pawan-katiyar-aesop <pawan@sqre1.com>
2019-10-18 09:31:09 +0530

    removed conflict

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+app = apps.get_app_config('website')
+
+for model_name, model in list(app.models.items()):
+    admin.site.register(model)
--- website/views.py
+++ website/views.py
@@ -7,9 +7,20 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "home.html"
 
+    def get_context_data(self, *args, **kwargs):
+        from website.models import JobPosting, Testimonials
+        ctx = super(HomePageView, self).get_context_data(*args, **kwargs)
+        ctx["job_postings"] = JobPosting.objects.all()
+        ctx["testimonials"] = Testimonials.objects.all()
+        return ctx
+
 
 class EnrollView(TemplateView):
     template_name = "enroll.html"
+
+    def post(self, request):
+        import pdb
+        pdb.set_trace()
 
 
 class AboutView(TemplateView):
